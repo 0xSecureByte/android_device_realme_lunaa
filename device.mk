df75fbd406702223623a18f54d05f283d2093e33
Karan Parashar <karan@pixelos.net>
2024-08-03 22:30:04 +0530

    lisa: Build libutils.vendor
    E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library "libutils.so" not found.
    
    Change-Id: I5bd9623fa7a25efd5c429bac3eca78e10855219e
    Signed-off-by: 0xSecureByte <chirantan.code@gmail.com>

--- device.mk
+++ device.mk
@@ -49,6 +49,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
