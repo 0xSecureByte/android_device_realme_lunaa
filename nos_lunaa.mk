9917e0ec575de3c69f3c122dff35da88b7fc2840
0xSecureByte <chirantan.code@gmail.com>
2025-01-16 10:08:10 +0530

    lunaa: Adapt to NeutronOS
    Change-Id: Ic0fe5fef13f14eb697fa32033019ea5802d18549
    Signed-off-by: 0xSecureByte <chirantan.code@gmail.com>

---
+++ nos_lunaa.mk
@@ -0,0 +1,49 @@
+# Copyright (C) 2024 NeutronOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (nos_lunaa,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/realme/lunaa/device.mk)
+
+# Inherit some NeutronOS vendor targets
+$(call inherit-product, vendor/neutron/configs/nos_version.mk)
+$(call inherit-product, vendor/neutron/target/product/neutronos-target.mk)
+
+PRODUCT_BRAND := realme
+PRODUCT_DEVICE := lunaa
+PRODUCT_MANUFACTURER := realme
+PRODUCT_MODEL := RMX3360
+PRODUCT_NAME := nos_lunaa
+
+PRODUCT_SYSTEM_DEVICE := RE54ABL1
+PRODUCT_SYSTEM_NAME := RMX3360
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3360-user 13 TP1A.220905.001 R.136e3d6-af7c-10d436 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3360/RE54ABL1:13/TP1A.220905.001/R.136e3d6-af7c-10d436:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+endif
